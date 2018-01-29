8b88dc193336c263273130ec58a2c3c995bd0e70
Mayra  Salazar <mayrasalazar@unitec.edu>
2018-01-29 15:15:18 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,12 +5,12 @@ def current_player(board)
 end
 
 def turn_count(board)
-   counter = 0 
-   board.each do |turn| 
-     if turn == "X" 
-       counter += 1 
-     elsif turn == "O" 
-       counter += 1 
-     end 
-   end 
+   counter = 0
+   board.each do |turn|
+     if turn == "X"
+       counter += 1
+     elsif turn == "O"
+       counter += 1
+     end
+   end
 end
