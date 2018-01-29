8bb990071d9268ec4a8a2db6aa353c6509e902bd
Mayra  Salazar <mayrasalazar@unitec.edu>
2018-01-29 15:16:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,11 @@
-
+def turn_count(board)
+  count=0
+  for value in board do
+    value!=" " ? count+=1: count = count
+  end
+  return count
+end
 
 def current_player(board)
   turn_count(board).even? ? "X":"O"
-end
-
-def turn_count(board)
-   counter = 0
-   board.each do |turn|
-     if turn == "X"
-       counter += 1
-     elsif turn == "O"
-       counter += 1
-     end
-   end
-   counter
 end
